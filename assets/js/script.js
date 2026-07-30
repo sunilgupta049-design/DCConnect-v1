dc4573e85e758a778720be3200e6c6317c60e9d7
sunilgupta049-design <sunil.gupta049@gmail.com>
2026-07-30 14:24:45 +0530

    Fix login page CSS

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const showPassword = document.getElementById("showPassword");
+const password = document.getElementById("password");
+
+if (showPassword && password) {
+    showPassword.addEventListener("change", function () {
+        if (this.checked) {
+            password.type = "text";
+        } else {
+            password.type = "password";
+        }
+    });
+}
